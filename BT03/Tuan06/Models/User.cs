b50bc3d1f24d0363aa4c37ef81bb0b0b93bb9116
dmn-05 <nnbdo45@gmail.com>
2025-10-10 02:20:32 +0700

    merge branch Tam 10/10/2025 2.20

--- BT03/Tuan06/Models/User.cs
+++ BT03/Tuan06/Models/User.cs
@@ -5,7 +5,7 @@
         public int UserID { get; set; }
         public string FullName { get; set; }
         public string UserName { get; set; }
-    public string UserPassword { get; set; } 
+        public string UserPassword { get; set; } 
         public string Phone { get; set; }
         public string Email { get; set; }
         public string UserAddress { get; set; }
