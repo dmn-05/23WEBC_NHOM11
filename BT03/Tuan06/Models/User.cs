aa34c4697ee1545b517c6f1f2036c644386a0142
dmn-05 <nnbdo45@gmail.com>
2025-10-10 01:27:40 +0700

    update model user

--- BT03/Tuan06/Models/User.cs
+++ BT03/Tuan06/Models/User.cs
@@ -4,7 +4,8 @@
     {
         public int UserID { get; set; }
         public string FullName { get; set; }
-        public string UserPassword { get; set; } 
+        public string UserName { get; set; }
+    public string UserPassword { get; set; } 
         public string Phone { get; set; }
         public string Email { get; set; }
         public string UserAddress { get; set; }
