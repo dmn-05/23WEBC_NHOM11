32e2bf48b7a8d7ff1843fc1701942db89766b356
QKhai2401 <0306231295@caothang.edu.vn>
2025-10-11 01:18:50 +0700

    11/10/2025 1:18

--- BT03/Tuan06/Models/Product.cs
+++ BT03/Tuan06/Models/Product.cs
@@ -2,13 +2,14 @@
 
 namespace Tuan06.Models {
   public class Product {
-    public int ProductID{ get; set; }
-    public string ProductName { get; set; }
-    public decimal Price { get; set; }
-    public decimal DiscountPrice { get; set; }
-    public string ProductImage { get; set; }
-    public string ProductDescription { get; set; }
-    public int CategoryID { get; set; }
-  }
+		public int ProductID { get; set; }
+		public string ProductName { get; set; }
+		public decimal Price { get; set; }
+		public decimal DiscountPrice { get; set; }
+		public string ProductImage { get; set; }
+		public string ProductDescription { get; set; }
+		public int CategoryID { get; set; }
+
+	}
 }
 
