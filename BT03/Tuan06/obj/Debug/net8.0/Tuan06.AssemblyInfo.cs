fcfac13b11b307477e79b0d47c29a33c08618c49
dmn-05 <nnbdo45@gmail.com>
2025-10-12 00:57:00 +0700

    update 12:54 12/10/2025

--- BT03/Tuan06/obj/Debug/net8.0/Tuan06.AssemblyInfo.cs
+++ BT03/Tuan06/obj/Debug/net8.0/Tuan06.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tuan06")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6feec0ac68e2aa8008892fa36a44eea4532944a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+854e2369b9ad9c963ba41d95e026854c7b695e9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tuan06")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tuan06")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
