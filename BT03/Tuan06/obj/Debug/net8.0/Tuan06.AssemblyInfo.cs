0cfcb741fcc7c51e46c79b8fe225a95e9c69a65e
dmn-05 <nnbdo45@gmail.com>
2025-10-16 13:35:14 +0700

    fix bug 16/10/2025

--- BT03/Tuan06/obj/Debug/net8.0/Tuan06.AssemblyInfo.cs
+++ BT03/Tuan06/obj/Debug/net8.0/Tuan06.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tuan06")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3077cdf84f28dbcf9b2dec787e720bd4bed0dbc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tuan06")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tuan06")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
